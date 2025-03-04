514ebec51599373ec61b7920972d92fbf14e2be6
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-04 18:02:54 -0300

    ✨ feat: create schedule component

---
+++ src/components/Schedule.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Schedule() {
+  return (
+    <section className="mt-8">
+      <h2 className="text-4xl font-bold text-center mb-8">
+        Calendario semanal
+      </h2>
+      <div className="px-8">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Lunes</th>
+                <th>Martes</th>
+                <th>Miércoles</th>
+                <th>Jueves</th>
+                <th>Sábado</th>
+                <th>Domingo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
+}
