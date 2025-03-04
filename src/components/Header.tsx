43bb7a09c93a60cd4e2ff07e7071a4fdbfc9be0b
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-04 18:02:42 -0300

    ✨ feat: create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
 import logoUtn from "../assets/logo-utn.png";
 export default function Header() {
   return (
-    <header>
+    <header className="py-2 px-8">
       <div className="flex flex-row items-center gap-2">
         <img src={logoUtn} className="h-12 w-12" />
         <div>
           <h1 className="text-2xl font-bold">Horarios Académicos</h1>
-          <p className="text-gray-700">
-            Gestiona tus horarios para planificar tu día académico
-          </p>
+          <p className="text-gray-700">Gestiona tus horarios en la UTN FRM</p>
         </div>
       </div>
     </header>
