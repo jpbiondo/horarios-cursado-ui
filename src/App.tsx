45f9badc9b86ceb943c63bb1a4002c6d7ff40b56
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-04 18:03:06 -0300

    ✨ feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Schedule from "./components/Schedule";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <Schedule />
+      <Footer />
+    </main>
   );
 }
 
