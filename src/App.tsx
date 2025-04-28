0284306002b78e80873e394eb14863086cf49b97
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-04-27 23:54:13 -0300

    style: remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Schedule from "./components/Schedule";
 
 function App() {
   return (
     <main>
-      <Header />
       <Schedule />
       <Footer />
     </main>
