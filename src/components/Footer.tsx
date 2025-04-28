366f414c5daea674815821359229d96dcd973a0d
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-04-27 23:55:46 -0300

    refactor: important data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-100 items-center p-4 mt-8">
-      <p className="mx-auto">
-        {new Date().getFullYear()} - Desarrollado por algún alumno random
-      </p>
+    <footer className="footer sm:footer-horizontal footer-center text-slate-800 text-md p-4">
+      <aside>
+        <p>{new Date().getFullYear()} &copy; Por Johnny The Man </p>
+      </aside>
     </footer>
   );
 }
