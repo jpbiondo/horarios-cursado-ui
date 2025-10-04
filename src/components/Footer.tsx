d6eafe381f52cfca4f0dc3cc286cbd9edb37c927
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-10-04 13:36:34 -0300

    shadcn final part: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center text-slate-800 text-md p-4">
-      <aside>
-        <p>{new Date().getFullYear()} &copy; Por Johnny The Man </p>
-      </aside>
+    <footer className="text-center text-sm text-muted-foreground p-4">
+      <p>{new Date().getFullYear()} &copy; Por Johnny The Man </p>
     </footer>
   );
 }
