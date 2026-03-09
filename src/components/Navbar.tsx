0ed41dddfadaf1d1f107f14b95ca151f98dcca54
Juan Pablo Biondo <jupbiondo@gmail.com>
2026-03-09 19:48:26 -0300

    add link to index on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar({ rightContent }: NavbarProps) {
     <header className="z-10 flex-shrink-0 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto flex h-14 items-center justify-between gap-2 px-1 sm:px-4">
         <div className="flex items-center gap-1">
-          <img src={logo} className="block h-14 dark:invert" />
+          <a href="/">
+            <img src={logo} className="block h-14 dark:invert" />
+          </a>
         </div>
 
-        <nav className="flex shrink-0 items-center gap-2">
-          {rightContent}
-        </nav>
+        <nav className="flex shrink-0 items-center gap-2">{rightContent}</nav>
       </div>
     </header>
   );
